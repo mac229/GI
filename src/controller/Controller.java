91c5441887814397a6e8c9e39cab02d8c58d20d5
mac229 <mac229@interia.eu>
2017-01-22 23:07:31 +0100

    Showing charts with real data

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,16 +1,19 @@
 package controller;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.StackedBarChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.input.MouseEvent;
 import main.CsvReader;
 import model.Area;
 import model.ProvinceData;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -23,52 +26,68 @@ public class Controller implements Initializable {
     @FXML
     private ListView<String> areasListView;
 
+    private List<Area> areas;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        List<Area> areas = CsvReader.readData();
+        areas = CsvReader.readData();
+        stackedBarChart.setAnimated(false);
 
         setSelectionMode();
-        setProvinceListView(areas);
-        setAreasListView(areas);
-
-        setCharts(areas);
+        setProvinceListView();
+        setAreasListView();
     }
 
-    private void setCharts(List<Area> areas) {
-        XYChart.Series dataSeries1 = new XYChart.Series();
-        dataSeries1.setName("Desktop");
-
-        dataSeries1.getData().add(new XYChart.Data("2014", 567));
-        dataSeries1.getData().add(new XYChart.Data("2015", 540));
-
-        stackedBarChart.getData().add(dataSeries1);
-
-        XYChart.Series dataSeries2 = new XYChart.Series();
-        dataSeries2.setName("Phone");
-
-        dataSeries2.getData().add(new XYChart.Data("2014", 65));
-        dataSeries2.getData().add(new XYChart.Data("2015", 120));
-
-        stackedBarChart.getData().add(dataSeries2);
-
-        XYChart.Series dataSeries3 = new XYChart.Series();
-        dataSeries3.setName("Tablet");
-
-        dataSeries3.getData().add(new XYChart.Data("2014", 23));
-        dataSeries3.getData().add(new XYChart.Data("2015", 36));
-
-        stackedBarChart.getData().add(dataSeries3);
+    @FXML
+    public void onProvinceClick(MouseEvent event) {
+        refreshChart();
     }
 
-    private void setProvinceListView(List<Area> areas) {
+    @FXML
+    public void onAreaClick(MouseEvent event) {
+        refreshChart();
+    }
+
+    private void setChart() {
+        ObservableList<String> selectedAreas = areasListView.getSelectionModel().getSelectedItems();
+        ObservableList<String> selectedProvinces = provincesListView.getSelectionModel().getSelectedItems();
+
+        List<XYChart.Series> seriesList = new ArrayList<>();
+        for (String areaName : selectedAreas) {
+            XYChart.Series dataSeries = new XYChart.Series();
+            dataSeries.setName(areaName);
+            seriesList.add(dataSeries);
+        }
+
+        for (XYChart.Series series : seriesList) {
+            String areaName = series.getName();
+            for (Area area : areas) {
+                if (areaName.equals(area.getName())) {
+                    for (ProvinceData province : area.getProvinceData()) {
+                        if (selectedProvinces.contains(province.getProvince())) {
+                            series.getData().add(new XYChart.Data(province.getProvince(), province.getValue2014()));
+                        }
+                    }
+                }
+            }
+        }
+
+        stackedBarChart.getData().addAll(seriesList);
+    }
+
+    private void refreshChart() {
+        stackedBarChart.getData().clear();
+        setChart();
+    }
+
+    private void setProvinceListView() {
         List<ProvinceData> provinceDataList = areas.get(0).getProvinceData();
         for (int i = 1; i < provinceDataList.size(); i++) {
             provincesListView.getItems().add(provinceDataList.get(i).getProvince());
         }
     }
 
-    private void setAreasListView(List<Area> areas) {
+    private void setAreasListView() {
         for (Area area : areas) {
             areasListView.getItems().add(area.getName());
         }
--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class Main extends Application {
 
     public static final int WIDTH = 1100;
-    public static final int HEIGHT = 600;
+    public static final int HEIGHT = 700;
 
     public static void main(String[] args) throws IOException {
         Application.launch(args);
@@ -19,7 +19,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../main.fxml"));
         Scene scene = new Scene(root, WIDTH, HEIGHT);
 
         primaryStage.setTitle("Ocena atrakcyjności województw na podstawie danych GUS");
